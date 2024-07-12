57a884d96d1dc4f4d867010552b2847caa3cc6fb
Danishjamil01 <danishjamil9065@gmail.com>
2024-07-12 13:55:38 +0530

    ddd

--- src/app/liabrary/liabrary.component.ts
+++ src/app/liabrary/liabrary.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import html2canvas from 'html2canvas';
+import { TabVisibilityService } from '../services/tab-visibility.service';
 
 @Component({
   selector: 'app-liabrary',
   templateUrl: './liabrary.component.html',
   styleUrl: './liabrary.component.scss'
 })
-export class LiabraryComponent {
-  // constructor() {
+export class LiabraryComponent implements OnInit {
+  constructor(private tab: TabVisibilityService) {
 
-  // }
+  }
+  ngOnInit(): void {
+    this.tab.setVisibility(false);
+  }
 
   // savePageAsImage() {
   //   const element = document.body;
@@ -23,6 +27,7 @@ export class LiabraryComponent {
   // }
 
   savePageAsImage() {
+    console.log('work');
     const element = document.body;
     html2canvas(element).then(canvas => {
       const imgData = canvas.toDataURL('image/png');
